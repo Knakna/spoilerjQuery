c06d887ad1db7a917ea5125f929857744923644e
knakna <kna@tut.by>
2021-10-09 18:44:14 +0300

    add elements of form

--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
-$(document).ready(function() {
-	$('.block__title').click(function(event) {
-		if($('.block').hasClass('one')){
+$(document).ready(function () {
+	// spoiler
+	$('.block__title').click(function (event) {
+		if ($('.block').hasClass('one')) {
 			$('.block__title').not($(this)).removeClass('active');
 			$('.block__text').not($(this).next()).slideUp(300);
 		}
 		$(this).toggleClass('active').next().slideToggle(300);
 	});
-
-	$('.header__burger').click(function(event) {
+	// burger
+	$('.header__burger').click(function (event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	//CHECKBOX
+	$.each($('.checkbox'), function (index, val) {
+		if ($(this).find('input').prop('checked') == true) {
+			$(this).addClass('active');
+		}
+	});
+	$(document).on('click', '.checkbox', function (event) {
+		if ($(this).hasClass('active')) {
+			$(this).find('input').prop('checked', false);
+		} else {
+			$(this).find('input').prop('checked', true);
+		}
+		$(this).toggleClass('active');
 
+		return false;
+	});
+
+	//RADIO
+	$.each($('.radiobuttons__item'), function (index, val) {
+		if ($(this).find('input').prop('checked') == true) {
+			$(this).addClass('active');
+		}
+	});
+	$(document).on('click', '.radiobuttons__item', function (event) {
+		$(this).parents('.radiobuttons').find('.radiobuttons__item').removeClass('active');
+		$(this).parents('.radiobuttons').find('.radiobuttons__item input').prop('checked', false);
+		$(this).toggleClass('active');
+		$(this).find('input').prop('checked', true);
+		return false;
+	});
 
 });
 
@@ -32,5 +62,43 @@ $(document).ready(function() {
 // 	$('.header__burger').click(function(event) {
 // 		$('.header__burger,.header__menu').toggleClass('active');
 // 		$('body').toggleClass('lock');
+// 	});
+// });
+
+
+
+// Перенесла в код выше стилизация  checkbox и radio
+
+// $(document).ready(function () {
+
+// 	//CHECKBOX
+// 	$.each($('.checkbox'), function (index, val) {
+// 		if ($(this).find('input').prop('checked') == true) {
+// 			$(this).addClass('active');
+// 		}
+// 	});
+// 	$(document).on('click', '.checkbox', function (event) {
+// 		if ($(this).hasClass('active')) {
+// 			$(this).find('input').prop('checked', false);
+// 		} else {
+// 			$(this).find('input').prop('checked', true);
+// 		}
+// 		$(this).toggleClass('active');
+
+// 		return false;
+// 	});
+
+// 	//RADIO
+// 	$.each($('.radiobuttons__item'), function (index, val) {
+// 		if ($(this).find('input').prop('checked') == true) {
+// 			$(this).addClass('active');
+// 		}
+// 	});
+// 	$(document).on('click', '.radiobuttons__item', function (event) {
+// 		$(this).parents('.radiobuttons').find('.radiobuttons__item').removeClass('active');
+// 		$(this).parents('.radiobuttons').find('.radiobuttons__item input').prop('checked', false);
+// 		$(this).toggleClass('active');
+// 		$(this).find('input').prop('checked', true);
+// 		return false;
 // 	});
 // });
